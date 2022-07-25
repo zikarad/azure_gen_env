2b0d532661a51e0b002d2a600d16e347167f1519
zikarad <radek.zika@gmail.com>
2022-07-25 20:19:23 +0200

    Some variables defined as sensitive

--- variables.tf
+++ variables.tf
@@ -32,18 +32,22 @@ variable "aws_secret_key" {}
 /* Azure */
 variable "azure_subscription_id" {
   type = string
+  sensitive = true
 }
 
 variable "azure_client_id" {
   type = string
+  sensitive = true
 }
 
 variable "azure_client_secret" {
   type = string
+  sensitive = true
 }
 
 variable "azure_tenant_id" {
   type = string
+  sensitive = true
 }
 
 variable "location" {
